3ee621ad8a74b02eece2c661d216d33fcd93b889
Azdevpp <azdevpp@gmail.com>
2022-10-30 20:42:26 +0200

    dateFormClass

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-card-form',
@@ -8,10 +9,20 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('', [
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration: new DateFormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
-      Validators.maxLength(10),
+      Validators.maxLength(3),
     ]),
   });
   constructor() {
@@ -20,4 +31,8 @@ export class CardFormComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('Form was Submited');
+  }
 }
